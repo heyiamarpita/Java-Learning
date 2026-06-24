66acdb6910ede3b2e7c90adefbff9c6b4edc64f2
heyiamarpita <heyiamarpita@gmail.com>
2026-06-24 11:36:23 +0600

    University practice problem

---
+++ Variable/Variable.java
@@ -0,0 +1,13 @@
+public class Variable {
+    public static void main(String[] args) {
+        String name = "Arpita Paul";
+        int roll = 1033;
+        String address = "Chittagong, Bangladesh";
+        boolean isStudent = true;
+        System.out.println("Name: " + name);
+        System.out.println("Roll: " + roll);
+        System.out.println("Address: " + address);
+        System.out.println("Is Student: " + isStudent);
+        System.out.println("It's " + isStudent + ", " + name + " is a student at Premier University.");
+    }
+}
